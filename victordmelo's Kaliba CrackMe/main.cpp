27e33319acaef1023ac43468637870d06badeabc
unknown <amore@t540p-amore.zobristinc.com>
2015-06-01 21:21:26 -0700

    First CrackMe

---
+++ victordmelo's Kaliba CrackMe/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * Keygen made for victordmelo's Kaliba CrackMe from Crackme.de
+ * Executable Download: http://www.crackmes.de/users/victordmelo/kaliba_crackme/
+ * Author: Adam L More
+ * Date: 6/1/2015
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    srand (time(NULL));
+    int val1 = rand()%999; //Random number serves as the first value in the key
+    int val2 = ((((((val1<<2)+val1)*2)+0x7D)*2)+1-0x15+ 0x58);
+    int val3 = (((val1*2) + 0x0FD)*2)+ 1 + val2 + 2;
+    cout << val1 << "a" << val2 << "a" << val3 << endl; //Key format is %d%c%d%c%d
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
